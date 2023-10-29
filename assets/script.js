40d15dd9fdbcf7bda5466152bc336acd225ce975
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2023-10-29 20:58:38 +0000

    Updated prompt logic to where it checks if the correct values are used

--- assets/script.js
+++ assets/script.js
@@ -98,19 +98,54 @@ var passSpecial
 function getPasswordOptions() {
   passLength = prompt("How long do you want your password.")
   if (passLength < 8) {
-    alert("Has to be higher than 8"); return;
+    alert("Has to be higher than 8"); return getPasswordOptions();
   } else if (passLength > 128) {
-    alert("Has to be less than 128"); return;
-  } else alert("Perfect, your password will be " + passLength + " characters long");
-  passLower = prompt("Do you want Lowercase. Y/N");
-  passUpper = prompt("Do you want Uppercase. Y/N");
-  passNum = prompt("Do you want Numbers. Y/N");
-  passSpecial = prompt("Do you want Special Characters. Y/N");
+    alert("Has to be less than 128"); return getPasswordOptions();
+  } else if (passLength > 8 && passLength < 128) {
+    alert("Perfect, your password will be " + passLength + " characters long");
+  } else return getPasswordOptions(); 
+  
+  passLower = prompt("Do you want Lowercase? Y/N")
+  if (passLower !== "Y" && passLower === "N") {
+    alert("Great, your password will have lowercase characters");
+  } else if (passLower !== "N" && passLower === "Y") {
+    alert("Great, your password will have lowercase characters");
+  } else if (passLower !== "N" || passLower === "Y") {
+    alert("Please select Y or N. Restarting"); return getPasswordOptions();
+  }
+  
+  passUpper = prompt("Do you want Uppercase? Y/N");
+  if (passUpper !== "Y" && passUpper === "N") {
+    alert("Great, your password will have uppercase characters");
+  } else if (passUpper !== "N" && passUpper === "Y") {
+    alert("Great, your password will have uppercase characters");
+  } else if (passUpper !== "N" || passUpper === "Y") {
+    alert("Please select Y or N. Restarting"); return getPasswordOptions();
+  }
+  
+  passNum = prompt("Do you want Numbers? Y/N");
+  if (passNum !== "Y" && passNum === "N") {
+    alert("Great, your password will have numerical characters");
+  } else if (passNum !== "N" && passNum === "Y") {
+    alert("Great, your password will have numerical characters");
+  } else if (passNum !== "N" || passNum === "Y") {
+    alert("Please select Y or N. Restarting"); return getPasswordOptions();
+  }
+  
+  passSpecial = prompt("Do you want Special Characters? Y/N");
+  if (passSpecial !== "Y" && passSpecial === "N") {
+    alert("Great, your password will have special characters");
+  } else if (passSpecial !== "N" && passSpecial === "Y") {
+    alert("Great, your password will have special characters");
+  } else if (passSpecial !== "N" || passSpecial === "Y") {
+    alert("Please select Y or N. Restarting"); return getPasswordOptions();
+  }
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  
+  var shuffledLower = lowerCasedCharacters.sort(() => 0.5 - Math.random());
+    var resultLower = shuffledLower.slice(0, passLower)
 }
 
 // Function to generate password with user input
@@ -133,4 +168,8 @@ function writePassword() {
 generateBtn.addEventListener('click', writePassword);
 
 getPasswordOptions();
-getRandom();
+console.log(passLower)
+console.log(passUpper)
+console.log(passNum)
+console.log(passSpecial)
+
